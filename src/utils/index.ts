714033a36fffc9ada96650d43696ae65cd71f398
MrJeleika <den932005@gmail.com>
2023-04-21 14:38:43 +0300

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,11 +12,8 @@ declare global {
 // format date to remove timezone 
 export const formatDate = (date: Date | string): string => {
 
-  console.log(String(date));
-  
-
-  let index = String(date).indexOf(' (')
-  if (String(date).includes(' (')) return String(date).substring(0, index)
+  let index = String(date).indexOf(' G')
+  if (String(date).includes(' G')) return String(date).substring(0, index)
   return String(date)
 }
 
