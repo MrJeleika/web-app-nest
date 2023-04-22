ccc10cd098770f66f5306a253f8ccff08bc7e809
MrJeleika <den932005@gmail.com>
2023-04-22 12:28:02 +0300

    test commands

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -11,51 +11,55 @@ const token = process.env.TOKEN
 @Injectable()
 export class TelegramService {
   private readonly bot: TelegramBot
-  private chatId: any
+  private chatId: number
 
-  constructor(private dayLessonsService: DayLessonsService){
-    this.bot = new TGB(token, {polling: true});
+  constructor(private dayLessonsService: DayLessonsService) {
+    this.bot = new TGB(token, { polling: true });
     moment.locale('uk')
-    
-    this.bot.addListener('message', async (msg) =>{
+
+    this.bot.addListener('message', async (msg) => {
 
       this.chatId = msg.chat.id;
-      const {dayLessons} = await this.dayLessonsService.findDayLesson(formatDate(new Date(new Date().setHours(0, 0, 0, 0))))
+      const { dayLessons } = await this.dayLessonsService.findDayLesson(formatDate(new Date(new Date().setHours(0, 0, 0, 0))))
       setInterval(() => this.sendScheduleOnTime(dayLessons), 60 * 1000)
     })
-    this.bot.onText(/^\/schedule$/ , async (msg) => {
+    this.bot.onText(/^\/schedule$/, async (msg) => {
       console.log(formatDate(new Date(new Date().setHours(0, 0, 0, 0))));
       this.chatId = msg.chat.id;
-      const {dayLessons} = await this.dayLessonsService.findDayLesson(formatDate(new Date(new Date().setHours(0, 0, 0, 0))))
-      
+      const { dayLessons } = await this.dayLessonsService.findDayLesson(formatDate(new Date(new Date().setHours(0, 0, 0, 0))))
+
       this.sendSchedule(dayLessons)
+    })
+    this.bot.onText(/^\/start$/, async (msg) => {
+      this.chatId = msg.chat.id;
+
     })
   }
 
-  sendScheduleOnTime(dayLessons:ILesson[]){
+  sendScheduleOnTime(dayLessons: ILesson[]) {
     const sendTime: string = '15:55'
-    
-    if(moment().format('LT') === sendTime){
+
+    if (moment().format('LT') === sendTime) {
       this.sendSchedule(dayLessons)
     }
   }
 
-  sendSchedule(dayLessons:ILesson[]){
+  sendSchedule(dayLessons: ILesson[]) {
     let firstGroupMessage: string = ''
     let secondGroupMessage: string = ''
-    
-    const makeStringMessage = (group: number, dayLesson: ILesson) =>{
-      let scheduleString:string = ''
-      if(dayLesson.group === group){
+
+    const makeStringMessage = (group: number, dayLesson: ILesson) => {
+      let scheduleString: string = ''
+      if (dayLesson.group === group) {
 
         scheduleString += dayLesson.time + ' - '
         scheduleString += `${moment(dayLesson.time, 'HH:mm').add(95, 'minutes').hours()}:${moment(dayLesson.time, 'HH:mm').add(95, 'minutes').minutes()} | `
         scheduleString += dayLesson.lessonName + ' | '
         scheduleString += dayLesson.type + ' | '
-        if(dayLesson.link){
+        if (dayLesson.link) {
           scheduleString += dayLesson.teacher + ' | '
-          scheduleString += '<a href="'+dayLesson.link+'">Посилання</a>' + '\n'
-        }else{
+          scheduleString += '<a href="' + dayLesson.link + '">Посилання</a>' + '\n'
+        } else {
           scheduleString += dayLesson.teacher + '\n'
         }
       }
@@ -63,15 +67,18 @@ export class TelegramService {
     }
 
     // Making a string message
-    dayLessons.sort((a,b) => a.time > b.time ? 1 : -1 ).forEach(dayLesson => {
-        firstGroupMessage += makeStringMessage(1, dayLesson)
-        secondGroupMessage += makeStringMessage(2, dayLesson)
+    dayLessons.sort((a, b) => a.time > b.time ? 1 : -1).forEach(dayLesson => {
+      firstGroupMessage += makeStringMessage(1, dayLesson)
+      secondGroupMessage += makeStringMessage(2, dayLesson)
     })
 
     this.bot.sendMessage(this.chatId, '<b>Пари на ' + moment().format('L') + ` ${daysOfWeek[new Date(new Date().setHours(0, 0, 0, 0)).getWeekDay() - 1]} ${new Date(new Date().setHours(0, 0, 0, 0)).getWeek()}:</b>\n\n`
-    + `<b>Перша підгрупа:</b>\n` + firstGroupMessage + '\n' 
-    + `<b>Друга підгрупа:</b>\n` + secondGroupMessage + '\n',
-    {parse_mode: "HTML", disable_web_page_preview: true});
+      + `<b>Перша підгрупа:</b>\n` + firstGroupMessage ? firstGroupMessage : 'Пар немає' + '\n'
+        + `<b>Друга підгрупа:</b>\n` + secondGroupMessage ? secondGroupMessage : 'Пар немає' + '\n',
+      { parse_mode: "HTML", disable_web_page_preview: true });
+  }
+  sendStartInfo(){
+    this.bot.sendMessage(this.chatId, 'Привіт 👀\n'+'<span class="tg-code">/schedule</span>', { parse_mode: "HTML", disable_web_page_preview: true })
   }
 
 }
